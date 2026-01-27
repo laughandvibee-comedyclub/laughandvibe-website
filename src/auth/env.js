f09005de15b7fa9b1a2df91ef79e7b2c487e6e8b
krutika <96387574+Krutika-Waghmare@users.noreply.github.com>
2026-01-27 20:31:29 +0530

    add action,methodPost to signup, add supabase url,variables

--- src/auth/env.js
+++ src/auth/env.js
@@ -1,4 +1,4 @@
 window.ENV = {
-  SUPABASE_URL: "https://YOUR_PROJECT_ID.supabase.co",
-  SUPABASE_ANON_KEY: "YOUR_PUBLIC_ANON_KEY"
+  SUPABASE_URL: "https://hmlsodnanowiotfoaurg.supabase.co",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhtbHNvZG5hbm93aW90Zm9hdXJnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwNzQyMTAsImV4cCI6MjA4NDY1MDIxMH0.9erF2cWb5m0i_JVakELOsG5WKhCs8oWn97ssFUFgruk"
 };
